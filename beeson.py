0396e9e96b1c19a6ccea32e4af8e89586f337a65
chrisma71 <chris.pengfei.ma@gmail.com>
2024-06-08 22:02:20 -0400

    Update beeson.py

--- beeson.py
+++ beeson.py
@@ -15,12 +15,41 @@ def open_tetrio():
     # Open the URL
     driver.get(url)
 
+def sign_in():
+    # Wait for the username input field to be visible and then enter the username
+    username_input = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "entry_username"))
+    )
+    username_input.send_keys("beeson")
+    print("Entered username: beeson")
+
     # Wait for the JOIN button to be clickable and then click it
     join_button = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.ID, "entry_button"))
     )
     join_button.click()
     print("JOIN button clicked.")
+
+    password_input = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "login_password"))
+    )
+    password_input.send_keys("beeson")
+    print("Entered Password")
+
+    login_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "login_button"))
+    )
+    login_button.click()
+    print("LOGIN button clicked.")
+
+    try:
+        cool_button = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//div[@class='oob_button flex-item pri' and text()='COOL!']"))
+        )
+        cool_button.click()
+        print("COOL! button clicked.")
+    except:
+        print("COOL! button not present, continuing.")
        
 def play_zen():
     # Wait for the SOLO button to be visible and then click it
@@ -72,12 +101,48 @@ def apply_config():
     handling_button.click()
     print("HANDLING button clicked.")
 
+    handling_arr_field = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "handling_arr_field"))
+    )
+    handling_arr_field.click()
+    handling_arr_field.clear()
+    handling_arr_field.send_keys("0")
+    print("ARR = 0F")
+
+    handling_das_field = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "handling_das_field"))
+    )
+    handling_das_field.click()
+    handling_das_field.clear()
+    handling_das_field.send_keys("1")
+    print("DAS = 1F")
+
+    handling_dcd_field = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "handling_dcd_field"))
+    )
+    handling_dcd_field.click()
+    handling_dcd_field.clear()
+    handling_dcd_field.send_keys("0")
+    print("DCD = 0F")
+
+    handling_sdf_field = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "handling_sdf_field"))
+    )
+    handling_sdf_field.click()
+    handling_sdf_field.clear()
+    handling_sdf_field.send_keys("41")
+    print("SDF = 41F")
+
+
+
+
 
 
 
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 open_tetrio()
+sign_in()
 apply_config()
 time.sleep(60)
 driver.quit()
